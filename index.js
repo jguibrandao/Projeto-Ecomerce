705d0a0239a45d5e3f77b8ea48f9eebccf871377
jguibrandao <jguibrandao@gmail.com>
2021-05-16 14:34:54 -0300

    Adicionado uma função que por algum motivo ainda não funciona, essa função serve para mostrar se o carrinho está vazio.

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ inicializarLoja = () => {
         </div>
         `;
     })
+    verificaQuantidade();
 }
 
 inicializarLoja();
@@ -48,6 +49,22 @@ atualizarCarrinho = () => {
             `;
         }
     })
+    verificaQuantidade();
+}
+
+
+function verificaQuantidade() {
+    var tem;
+    var estadoCarrinho = document.getElementsByClassName("estado-carrinho");
+    for(var i = 0; i < itens.length; i++) {
+        if(itens[i].quantidade <= 0) {
+            tem = false;
+        } else {tem = true}
+    }
+
+    if(tem == false) {
+        estadoCarrinho.innerHTML = `<p>Seu carrinho está vazio<p>`;
+    }
 }
 
 var links = document.getElementsByClassName("link-carrinho");
